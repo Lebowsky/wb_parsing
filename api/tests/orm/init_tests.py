8c2228629c8c1c49eeb95e58dc085b3c475823cc
And <ase@crimea.com>
2024-02-17 15:37:40 +0300

    add user ORM tests

---
+++ api/tests/orm/init_tests.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from db.tables import Base
+
+
+async def create_sessionmaker():
+    engine = create_async_engine(
+        'sqlite+aiosqlite:///:memory:',
+        future=True,
+    )
+
+    async with engine.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    async_sessionmaker = sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession
+    )
+
+    return async_sessionmaker
